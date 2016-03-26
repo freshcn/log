d7d636b24dbf2876f7475cff9377f9d81aa0ac31
chenzhouyu <chenzhouyu@qq.com>
2016-03-26 17:29:01 +0800

    修改程序文件为标准语法

--- log.go
+++ log.go
@@ -73,9 +73,9 @@ func init() {
 	}()
 }
 
-// 写入数据到文件中
-func (this *data) Write(p []byte) (int, error) {
-	if this.debug {
+// Write 写入数据到文件中
+func (d *data) Write(p []byte) (int, error) {
+	if d.debug {
 		pStr := string(p)
 		pStr = strings.Replace(pStr, fmt.Sprintf("[%s]", ERROR), fmt.Sprintf("[\033[31m%s\033[0m]", ERROR), 1)
 		pStr = strings.Replace(pStr, fmt.Sprintf("[%s]", PANIC), fmt.Sprintf("[\033[31m%s\033[0m]", PANIC), 1)
@@ -83,17 +83,17 @@ func (this *data) Write(p []byte) (int, error) {
 		pStr = strings.Replace(pStr, fmt.Sprintf("[%s]", INFO), fmt.Sprintf("[\033[32m%s\033[0m]", INFO), 1)
 		fmt.Print(pStr)
 	}
-	return this.filePath.Write(p)
+	return d.filePath.Write(p)
 }
 
-// 获取当前正在写入的日志文件名
-func (this *data) FileName() string {
-	return filepath.Base(this.filePath.Name())
+// FileName 获取当前正在写入的日志文件名
+func (d *data) FileName() string {
+	return filepath.Base(d.filePath.Name())
 }
 
-// 关闭文件指针
-func (this *data) CloseFile() {
-	this.filePath.Close()
+// CloseFile 关闭文件指针
+func (d *data) CloseFile() {
+	d.filePath.Close()
 }
 
 // 设置是否为 debug 模式
@@ -101,31 +101,31 @@ func SetDebug(debugs bool) {
 	logData.debug = debugs
 }
 
-// 写入错误
+// Error 写入错误
 func Error(msg ...interface{}) {
 	_, filePath, line, _ := runtime.Caller(1)
 	log.Printf("[%s] %s %s:%d %s", ERROR, time.Now().Format("2006-01-02 15:04:05"), strings.Replace(filePath, build.Default.GOPATH, "", 1), line, fmt.Sprint(msg...))
 }
 
-// 写入信息
+// Info 写入信息
 func Info(msg ...interface{}) {
 	_, filePath, line, _ := runtime.Caller(1)
 	log.Printf("[%s] %s %s:%d %s", INFO, time.Now().Format("2006-01-02 15:04:05"), strings.Replace(filePath, build.Default.GOPATH, "", 1), line, fmt.Sprint(msg...))
 }
 
-// 写入警告
+// Warring 写入警告
 func Warring(msg ...interface{}) {
 	_, filePath, line, _ := runtime.Caller(1)
 	log.Printf("[%s] %s %s:%d %s", WARRING, time.Now().Format("2006-01-02 15:04:05"), strings.Replace(filePath, build.Default.GOPATH, "", 1), line, fmt.Sprint(msg...))
 }
 
-// 显示 panic
+// Panic 显示 panic
 func Panic(msg ...interface{}) {
 	_, filePath, line, _ := runtime.Caller(1)
 	log.Panicf("[%s] %s %s:%d %s", PANIC, time.Now().Format("2006-01-02 15:04:05"), strings.Replace(filePath, build.Default.GOPATH, "", 1), line, fmt.Sprint(msg...))
 }
 
-// 获取日志文件
+// logFile 获取日志文件
 func logFile() (*os.File, error) {
 	rootDir := fmt.Sprintf("%s/log/", runDir)
 	logFilePath := fmt.Sprintf("%s%s.log", rootDir, time.Now().Format("2006-01-02"))
@@ -148,7 +148,7 @@ func logFile() (*os.File, error) {
 	return os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 }
 
-// 获取程序的运行目录
+// RunDir 获取程序的运行目录
 func RunDir() string {
 	rootDir, err := exec.LookPath(os.Args[0])
 	if err != nil {

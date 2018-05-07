548f4c5aeb04181d6196e1d375d5f63505066b19
chenzhouyu <chenzhouyu@qq.com>
2018-05-08 00:25:11 +0800

    添加更多的可配置项

--- log_test.go
+++ log_test.go
@@ -1,16 +1,20 @@
 package log
+
 import (
-	"testing"
 	"fmt"
-	"time"
+	"testing"
 )
 
 func TestLogFile(t *testing.T) {
 	file, _ := logFile()
 	fmt.Println(file.Name())
-	SetDebug(true)
+	SetConfig(Config{
+		ShowFilePath:  true,
+		ShortFilePath: true,
+		GOPATHDeep:    2,
+		Debug:         true,
+	})
 	Error("这是一个错误")
 	Warring("这是一个警告")
 	Info("这是一个信息")
-	time.Sleep(100*time.Second)
 }

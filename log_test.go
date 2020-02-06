a7e46b1cc7f3f80af1a59afef61eab32dcf3d6a2
chenzhouyu <chenzhouyu@qq.com>
2020-02-06 13:43:17 +0800

    调整日志测试用例

--- log_test.go
+++ log_test.go
@@ -2,12 +2,31 @@ package log
 
 import (
 	"fmt"
+	"io/ioutil"
 	"testing"
 )
 
-func TestLogFile(t *testing.T) {
+func TestLogFileDebugFlase(t *testing.T) {
 	file, _ := logFile()
 	fmt.Println(file.Name())
+	SetConfig(Config{
+		ShowFilePath:  true,
+		ShortFilePath: true,
+		GOPATHDeep:    2,
+		Debug:         false,
+	})
+	Error("这是一个错误")
+	Warring("这是一个警告")
+	Info("这是一个信息")
+
+	content, err := ioutil.ReadFile(file.Name())
+	if err != nil {
+		content = []byte(err.Error())
+	}
+	fmt.Println("File Content: \n", string(content))
+}
+
+func TestLogFileDebugTrue(t *testing.T) {
 	SetConfig(Config{
 		ShowFilePath:  true,
 		ShortFilePath: true,
